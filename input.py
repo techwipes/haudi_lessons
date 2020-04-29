3a83eaa91e8da7d130b86a07b24fcc8290164599
Mitya <techwipes@gmail.com>
2020-04-29 17:21:05 +0500

    add lesson about input

---
+++ input.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+basic lesson about input
+
+"""
+
+name = input ("Enter your name: ")
+age = input ("Enter your age: ")
+
+print("hello, " + name + "!")
+print("You are " + age + " years old!")
+
