257e16f05bd8f96a15d302a8d38d41e60593f7df
Mitya <techwipes@gmail.com>
2020-04-29 17:15:16 +0500

    first init, basic

---
+++ basic.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+
+This is a first script file in learning course.
+"""
+
+
+# int 
+number = 5
+age = 25
+
+# float
+fnumber = 5.5
+
+# str
+name = "Ivan"
+
+# bool
+result = True
+
+
+# Экранирование
+print("This is a \"bad\" man " + name)
+
+# Перенос строки
+print("Hello \nWorld")
+
+# Concatenate and Typecasting
+print("Hello, my name is " + name)
+print("I am " + str(age) + " years old")
+
+
+
