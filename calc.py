99b9b52bd12a872b650fa8e889d8434f86fbf097
Mitya <techwipes@gmail.com>
2020-04-30 19:16:01 +0500

    add calc, first init, add variables

---
+++ calc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+basic calc
+
+"""
+
+operator = input("What do you want to do? (+, -, *, / : ")
+
+a= input("Enter the first number: ")
+b= input("Enter the second number: ")
