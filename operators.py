c5bb43667d8b64debd4b8414a17b2117e916d717
Mitya <techwipes@gmail.com>
2020-04-30 11:11:33 +0500

    add basic operators

---
+++ operators.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+ basic operators
+
+ +, -, *, /, **, %
+"""
+
+a = 5
+b = 10
+
+#
+c = a + b
+
+print( c )
+
+#
+c = a - b
+
+print( c )
+
+
+#
+c = a * b
+
+print( c )
+
+#
+c = a / b
+
+print( c )
+
+#
+c = a ** 2
+
+print( c )
+
+#
+c = a % b
+
+print( c )
+
+#
+a = -a
+
+print( a )
+
+#
+d = 5.65
+
+print( round(d) )
+
+
+
+
+
