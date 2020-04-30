f154103f5ba00b65386a39f36a95cfc0a81da3b8
Mitya <techwipes@gmail.com>
2020-04-30 19:27:08 +0500

    add operators

--- calc.py
+++ calc.py
@@ -7,5 +7,21 @@ basic calc
 
 operator = input("What do you want to do? (+, -, *, / : ")
 
-a= input("Enter the first number: ")
-b= input("Enter the second number: ")
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+if operator == "+":
+    c = a + b
+    
+elif operator == "-":
+    c = a - b
+    
+elif operator == "*":
+    c = a * b
+
+elif operator == "/":
+    c = a / b
+else:
+    print("Error! Incorrect Arithmetic Operator! ")
+
+print ("Result: = " + str(c) )    
