4926a50922563d06fb03a16e335810660e47d699
Mitya <techwipes@gmail.com>
2020-05-01 19:27:47 +0500

    add weather details and variables

--- weather_info.py
+++ weather_info.py
@@ -7,11 +7,22 @@ script for getting weather info
 
 import pyowm
 
-owm = pyowm.OWM('08ba5600a00017c0ee587931bfcfefa2')
+owm = pyowm.OWM('08ba5600a00017c0ee587931bfcfefa2', language = "ru")
 
-city = input("What city do you wonna check?")
+city = input("What city do you wonna check? ")
 
 # Search for current weather in city 
 observation = owm.weather_at_place(city)
 w = observation.get_weather()
-print(w)
+
+wind = w.get_wind()                
+humidity = w.get_humidity()            
+temperature = w.get_temperature('celsius')
+
+print(" In city " + city + " " + w.get_detailed_status() + " now ")
+print(" Temperature is: " + str(temperature) )
+print(" Wind is " + str(wind) + "now ")
+print(" Humidity is " + str(humidity) + "now ")
+
+
+
