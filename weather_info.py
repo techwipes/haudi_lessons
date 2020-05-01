93688cdd512897c75396e88b7a874e9c60eab21d
Mitya <techwipes@gmail.com>
2020-05-01 19:09:23 +0500

    add API and basic module pyowm

--- weather_info.py
+++ weather_info.py
@@ -8,3 +8,10 @@ script for getting weather info
 import pyowm
 
 owm = pyowm.OWM('08ba5600a00017c0ee587931bfcfefa2')
+
+city = input("What city do you wonna check?")
+
+# Search for current weather in city 
+observation = owm.weather_at_place(city)
+w = observation.get_weather()
+print(w)
